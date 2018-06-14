d06ac7e2e3630c8535c94074e08bc6bc26b3decd
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-06-14 14:07:56 +0530

    layout added

---
+++ AlarmManager/Startup/app/src/main/java/com/ayush171196/Startup/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ayush171196.Startup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun BuSetTime(view: View){
+
+    }
+}
