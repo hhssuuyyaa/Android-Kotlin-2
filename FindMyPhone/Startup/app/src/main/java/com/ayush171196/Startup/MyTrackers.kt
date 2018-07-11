cd9b7ad5654d87975d23f22e685e38f43e055317
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-07-11 17:39:50 +0530

    offline mobile tracker

--- FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MyTrackers.kt
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MyTrackers.kt
@@ -1,16 +1,32 @@
 package com.ayush171196.Startup
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.text.Layout
+import android.view.*
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_my_trackers.*
+import kotlinx.android.synthetic.main.contact_ticket.view.*
 
 class MyTrackers : AppCompatActivity() {
 
+    var adapter:ContactAdapter?=null
+    var listOfContact=ArrayList<UserContact>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_trackers)
+        dummyData()
+        adapter = ContactAdapter(this,listOfContact)
+        lvContactList.adapter = adapter
+    }
+
+    //for debug
+    fun dummyData() {
+        listOfContact.add(UserContact("Ayush", "8178826886"))
+        listOfContact.add(UserContact("Anshul", "8447770899"))
+        listOfContact.add(UserContact("Papa", "9810024409"))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -35,5 +51,36 @@ class MyTrackers : AppCompatActivity() {
             }
         }
         return true
+    }
+    class ContactAdapter:BaseAdapter {
+
+        var listOfContact=ArrayList<UserContact>()
+        var context:Context?=null
+        constructor(context:Context,listOfContact:ArrayList<UserContact>){
+            this.context=context
+            this.listOfContact=listOfContact
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val userContact = listOfContact[position]
+            val inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val contactTicketView = inflator.inflate(R.layout.contact_ticket, null)
+            contactTicketView.tvName.text= userContact.name
+            contactTicketView.tvPhoneNumber.text = userContact.phoneNumber
+            return contactTicketView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listOfContact[position]
+            //we dont need this part
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listOfContact.size
+        }
     }
 }
---
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/UserContact.kt
@@ -0,0 +1,10 @@
+package com.ayush171196.Startup
+
+class UserContact{
+    var name:String?=null
+    var phoneNumber:String?=null
+    constructor(name:String,phoneNumber:String){
+        this.name=name
+        this.phoneNumber=phoneNumber
+    }
+}
