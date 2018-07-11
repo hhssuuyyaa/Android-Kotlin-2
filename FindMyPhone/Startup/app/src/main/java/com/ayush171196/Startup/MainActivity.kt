8c86364a76a61a374e3c809eefee32f7699078bd
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-07-11 12:30:52 +0530

    offline mobile tracker

--- FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MainActivity.kt
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.ayush171196.Startup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
@@ -14,4 +17,29 @@ class MainActivity : AppCompatActivity() {
         userData.loadPhoneNumber()
     }
 
+    //To use menu in our project we need two methods and one of them is below
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //Loads menu in activity when this method calls up
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item!!.itemId){
+            R.id.addTracker ->{
+                val intent = Intent(this,MyTrackers::class.java)
+                startActivity(intent)
+            }
+            R.id.help ->{
+                //TODO:: ask for help from friend
+            }
+            else ->{
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        return true
+    }
 }
--- FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MyTrackers.kt
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MyTrackers.kt
@@ -1,12 +1,39 @@
 package com.ayush171196.Startup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MyTrackers : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_trackers)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //Loads menu in activity when this method calls up
+        val inflater = menuInflater
+        inflater.inflate(R.menu.tracker_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item!!.itemId){
+            R.id.finishActivity ->{
+                finish()
+            }
+            R.id.addContact ->{
+                //TODO:: ask new Contact
+            }
+            else ->{
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        return true
     }
 }
