b6df13101ca1f488eddf0051ada476e273fe7c59
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-07-13 19:04:02 +0530

    "Offline Mobile Tracker"

--- FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/Login.kt
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/Login.kt
@@ -3,13 +3,41 @@ package com.ayush171196.Startup
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+
+
 
 class Login : AppCompatActivity() {
-
+    var mAuth:FirebaseAuth?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        signInAnonymously()
+    }
+
+    fun signInAnonymously(){
+        mAuth!!.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(applicationContext, "Authentication Succesful.",
+                                Toast.LENGTH_SHORT).show()
+                        val user = mAuth!!.getCurrentUser()
+                    } else {
+                        Toast.makeText(applicationContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 
     fun buRegisterEvent(view: View){
