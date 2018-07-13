95f91b463654c597c87d080fcee12ffb7411d8de
Ayush Khanna <ayushkhanna1996@gmail.com>
2018-07-14 01:27:33 +0530

    "Offline Mobile Tracker"

--- FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MainActivity.kt
+++ FindMyPhone/Startup/app/src/main/java/com/ayush171196/Startup/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.ayush171196.Startup
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
+import android.widget.AdapterView
+import android.widget.BaseAdapter
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.contact_ticket.view.*
 
 class MainActivity : AppCompatActivity() {
-
+    var adapter: MyTrackers.ContactAdapter?=null
+    var listOfContact=ArrayList<UserContact>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val userData = UserData(this)
         userData.loadPhoneNumber()
+
+        dummyData()
+
+        adapter = MyTrackers.ContactAdapter(this, listOfContact)
+        lvContactList.adapter = adapter
+        lvContactList.onItemClickListener= AdapterView.OnItemClickListener{ parent, view, position, id ->
+            val userInfo = listOfContact[position]
+            UserData.myTrackers.remove(userInfo.phoneNumber)
+        }
+    }
+
+    //for debug
+    fun dummyData() {
+        listOfContact.add(UserContact("Ayush", "8178826886"))
+        listOfContact.add(UserContact("Anshul", "8447770899"))
+        listOfContact.add(UserContact("Papa", "9810024409"))
+
     }
 
     //To use menu in our project we need two methods and one of them is below
@@ -41,5 +63,37 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    class ContactAdapter: BaseAdapter {
+
+        var listOfContact=ArrayList<UserContact>()
+        var context: Context?=null
+        constructor(context: Context, listOfContact:ArrayList<UserContact>){
+            this.context=context
+            this.listOfContact=listOfContact
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val userContact = listOfContact[position]
+            val inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val contactTicketView = inflator.inflate(R.layout.contact_ticket, null)
+            contactTicketView.tvName.text= userContact.name
+            contactTicketView.tvPhoneNumber.text = userContact.phoneNumber
+            return contactTicketView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listOfContact[position]
+            //we dont need this part
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listOfContact.size
+        }
     }
 }
